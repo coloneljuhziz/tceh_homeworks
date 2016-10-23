8a44253bfe58a0f5977128fc1e7acb8f8cd2e649
Dmitry Panchenko <d.e.panchenko@gmail.com>
2016-10-23 18:20:47 +0300

    doesn't work still...

--- qr_code/app.py
+++ qr_code/app.py
@@ -1,21 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
-import json, pyqrcode
+import qrcode
 
-app = Flask(__name__)
-
-
-class Qrcode():
-    def __init__(self, text):
-        self.text = text
-
-    def __repr__(self):
-        return str(dict((key, getattr(self, key)) for key in dir(self) if key not in dir(self.__class__)))
+app = Flask(__name__, static_folder='static')
 
 
-@app.route('/', methods=['POST'])
+# class Qr_code():
+#     def __init__(self, text):
+#         self.text = text
+#
+#     def __repr__(self):
+#         return str(dict((key, getattr(self, key)) for key in dir(self) if key not in dir(self.__class__)))
+#
+#     def generate_image(self, text):
+#         qr = pyqrcode.create(text)
+#         qr.png('/static/user_gen_qrcode.png', scale=10)
+
+
+@app.route('/', methods=['GET','POST'])
 def main():
-    user_string = request.form['user_string']
-    return render_template('main.html', user_string = user_string)
+    qr_string = ''
+    qr_code = None
+    if 'user_string' in request.form:
+        qr_string = request.form['user_string']
+        qr_code = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr_code.add_data(qr_string)
+        qr_code.make(fit=True)
+        img = qr_code.make_image()
+        img.save('/static/user_gen_qrcode.png')
+    else:
+        print('Wrong! It\'s all wrong!!')
+
+    return render_template('main.html', qrcode=qr_code)
 
 
 if __name__ == '__main__':
